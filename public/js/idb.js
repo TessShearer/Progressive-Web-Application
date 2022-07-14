9ddd8d3269f95c20c43706d2ce2a2e50c62abde1
TessShearer <shearer.tess@gmail.com>
2022-07-14 15:47:46 -0600

    service worker and manifest

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,19 +1,16 @@
 //  Script tag for this: <script src="./js/idb.js"></script>
 
 
-// create variable to hold db connection
 let db;
-// establish a connection to IndexedDB database called 'pizza_hunt' and set it to version 1
 const request = indexedDB.open('pwa', 1);
 
-// this event will emit if the database version changes (nonexistant to version 1, v1 to v2, etc.)
-request.onupgradeneeded = function (event) {
+request.onupgradeneeded = (event) => {
     const db = event.target.result;
     db.createObjectStore('new_pwa', { autoIncrement: true });
 };
 
 // upon a successful 
-request.onsuccess = function (event) {
+request.onsuccess = (event) => {
     db = event.target.result;
     if (navigator.onLine) {
         uploadTransaction();
@@ -26,31 +23,22 @@ request.onerror = function (event) {
 
 //function for submitting with no connection
 function saveRecord(record) {
-    // open a new transaction with the database with read and write permissions 
     const transaction = db.transaction(['new_pwa'], 'readwrite');
-
-    // access the object store for `new_pwa`
     const pwaObjectStore = transaction.objectStore('new_pwa');
-
-    // add record to your store with add method
     pwaObjectStore.add(record);
-}
+};
 
 function uploadTransaction() {
-    // open a transaction on your db
     const transaction = db.transaction(['new_pwa'], 'readwrite');
 
-    // access your object store
     const pwaObjectStore = transaction.objectStore('new_pwa');
 
-    // get all records from store and set to a variable
     const getAll = pwaObjectStore.getAll();
 
-    // upon a successful .getAll() execution, run this function
     getAll.onsuccess = function () {
-        // if there was data in indexedDb's store, let's send it to the api server
+        // if there was data in indexedDb's store, send to the api server
         if (getAll.result.length > 0) {
-            fetch('/api/transaction', {
+            fetch('/api/transaction/bulk', {
                 method: 'POST',
                 body: JSON.stringify(getAll.result),
                 headers: {
@@ -63,11 +51,8 @@ function uploadTransaction() {
                     if (serverResponse.message) {
                         throw new Error(serverResponse);
                     }
-                    // open one more transaction
                     const transaction = db.transaction(['new_pwa'], 'readwrite');
-                    // access the new_pizza object store
                     const pwaObjectStore = transaction.objectStore('new_pwa');
-                    // clear all items in your store
                     pwaObjectStore.clear();
 
                     alert('All saved transactions have been submitted');
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3001;
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1/budget";
 
 const app = express();
 
@@ -19,7 +19,7 @@ app.use(express.static("public"));
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useFindAndModify: false
-});
+}).then(()=>{console.log('connected to database')}).catch((err)=>{console.log('not connected to the database because: ',err)});
 
 // routes
 app.use(require("./routes/api.js"));
